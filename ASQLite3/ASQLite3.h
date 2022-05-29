f2a2e43422cb52136f356eb4cdf8286b9ad05e41
Ihor Myroniuk <ihormyroniuk@gmail.com>
2022-05-29 10:37:20 +0300

    no message

---
+++ ASQLite3/ASQLite3.h
@@ -0,0 +1,18 @@
+//
+//  ASQLite3.h
+//  ASQLite3
+//
+//  Created by Ihor Myroniuk on 29.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASQLite3.
+FOUNDATION_EXPORT double ASQLite3VersionNumber;
+
+//! Project version string for ASQLite3.
+FOUNDATION_EXPORT const unsigned char ASQLite3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASQLite3/PublicHeader.h>
+
+
